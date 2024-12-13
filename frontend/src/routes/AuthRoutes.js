b92d8a54b49c579ffa44389e56973db4292df6a6
juanpab05 <165406198+juanpab05@users.noreply.github.com>
2024-12-13 18:58:19 -0500

    AuthRoutes para registro de estudiante y profesor

--- frontend/src/routes/AuthRoutes.js
+++ frontend/src/routes/AuthRoutes.js
@@ -1,11 +1,15 @@
 import { Home } from '../pages';
 import Login from '../pages/Home/Login/Login';
-import Registro from '../pages/Home/Registro/Registro,';
+import RegistroProf from '../pages/Home/RegistroProf/RegistroProf';
+import RegistroEst from '../pages/Home/RegistroEst/RegistroEst';
+import OlvidarContraseña from '../pages/Home/OlvidarContraseña/OlvidarContraseña';
 
 const AuthRoutes = [
     { path: '/', element: <Home/>},
     { path: '/login', element: <Login /> },
-    { path: '/registro', element: <Registro /> },
+    { path: '/registroProf', element: <RegistroProf/> },
+    { path: '/registroEst', element: <RegistroEst /> },
+    { path: '/olvidarContraseña', element: <OlvidarContraseña /> }
 ];
 
 export default AuthRoutes;
