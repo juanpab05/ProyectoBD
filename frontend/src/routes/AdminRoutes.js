65020c4fbf772028d5e2297597cb837aae689a04
David Santiago Guerrero <144239804+DavidSantiagoGuerrero@users.noreply.github.com>
2024-12-13 17:22:24 -0500

    Update AdminRoutes.js
    Actualización de las rutas

--- frontend/src/routes/AdminRoutes.js
+++ frontend/src/routes/AdminRoutes.js
@@ -4,6 +4,7 @@ import Reportes from '../pages/Reportes';
 import InformePreguntas from '../pages/InformePreguntas/InformePreguntas';
 import CrearPreguntas from '../pages/GestionClases/index';
 import CursosProfesor from '../pages/Curso';
+import GenerateQr from '../pages/CodigoQR';
 
 const AdminRoutes = [
     { path: '/asistencia', element: <Asistencia /> },
@@ -11,6 +12,7 @@ const AdminRoutes = [
     { path: '/reportes', element: <Reportes /> },
     { path: '/informepreguntas', element: <InformePreguntas /> },
     { path: '/crearpreguntas', element: <CrearPreguntas /> },
+    { path: '/codigoQR', element: <GenerateQr/> },
     { path: '/:nombreUsuario/cursos', element: <CursosProfesor /> }
 ];
 
